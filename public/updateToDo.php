406ea11e309268e723851f847727b095c9793d36
Inese Ciniņa <inese.cinina@gmail.com>
2019-12-11 12:01:09 +0200

    update

--- public/updateToDo.php
+++ public/updateToDo.php
@@ -5,24 +5,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $todo_id = $_POST['update'];
     $event = $_POST['event'];
-    $description = $_POST['description']; 
+    $description = $_POST['description'];
     $doing_date = $_POST['doing_date'];
 
+    
     $stmt = $conn->prepare("UPDATE `to_do_list`
         SET `event` = (:event),
             `description` = (:description),
-            `doing_date` = (:doing_date),
-        
+            `doing_date` = (:doing_date)
         WHERE `to_do_list`.`id` = (:todoid)");
 
     $stmt->bindParam(':todoid', $todo_id);
     $stmt->bindParam(':event', $event);
     $stmt->bindParam(':description', $description);
     $stmt->bindParam(':doing_date', $doing_date);
-    
 
     $stmt->execute();
-    //we go to our index.php or rather our root
     header('Location: /');
 } else {
     echo "That was not a POST, most likely GET";
